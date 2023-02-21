81cbbb1d900e3da2817b5a7d3a493b9ee3483c60
unknown <dollorinfotech@gmail.com>
2023-02-21 09:37:52 +0530

    new

---
+++ src/components/ui/Layouts.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+import { Col, Nav, Row } from 'react-bootstrap'
+import Header from './Header'
+import Footer from './Footer'
+import Aside from './Aside'
+
+export default function Layouts() {
+
+    return (
+        <>
+            <Header />
+            <main>
+                    <Row className="h-100">
+                        <Col sm={2} className="a_tbdr h-100">
+                            <Aside />
+                        </Col>
+                        <Col sm={10} className="a_tbdr h-100">
+                            <Outlet />
+                        </Col>
+                    </Row>
+                </main> 
+            <Footer />
+        </>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+export default class Home extends Component {
+    //1. Property
+
+    //2. Constructor
+
+
+    //3. Methods
+
+    render() {
+        return (
+            <>
+               <section>Home Section</section>
+            </>
+        )
+    }
+}
